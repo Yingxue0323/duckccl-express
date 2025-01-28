1670e413844e15d3335de2b5aba65f6555984ee9
Ying <yingxue0323@gmail.com>
2025-01-28 12:03:34 +1100

    fix: code2session call

--- src/utils/wechat.ts
+++ src/utils/wechat.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import logger from './logger';
+import { config } from '../configs';
 
 interface WxSession {
   session_key: string;
@@ -10,25 +11,28 @@ interface WxSession {
 }
 
 export async function code2Session(code: string): Promise<WxSession> {
-  const url = 'https://api.weixin.qq.com/sns/jscode2session';
-  const params = {
-    appid: process.env.WX_APPID,
-    secret: process.env.WX_SECRET,
-    js_code: code,
-    grant_type: 'authorization_code'
-  };
-
   try {
-    const { data } = await axios.get<WxSession>(url, { params });
-    
+    const response = await axios.get('https://api.weixin.qq.com/sns/jscode2session', {
+      params: {
+        appid: config.wx.appId,
+        secret: config.wx.appSecret,
+        js_code: code,
+        grant_type: 'authorization_code'
+      }
+    });
+
+    const data = response.data;
     if (data.errcode) {
-      logger.error('微信登录失败:', data);
-      throw new Error(data.errmsg || '微信登录失败');
+        logger.error('微信接口jscode2session返回错误:', {
+          errcode: data.errcode,
+          errmsg: data.errmsg
+        });
+        throw data;
     }
 
     return data;
-  } catch (error) {
+  } catch (error: any) {
     logger.error('调用微信接口失败:', error);
-    throw new Error('微信服务异常');
+    throw new Error('微信服务调用失败');
   }
 } 
