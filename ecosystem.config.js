28c1fc1e8cb742581950b168afe980efbbb7639f
Ying <yingxue0323@gmail.com>
2025-03-26 15:20:26 +1100

    Updated deploy.yml

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,23 @@ module.exports = {
     apps: [{
       name: 'koala',
       script: './dist/app.js',
-      instances: 1,
+      instances: 2,
       autorestart: true,
       watch: false,
       exec_mode: 'cluster',
+      wait_ready: true,
+      listen_timeout: 60000,
+      kill_timeout: 5000,
       env: {
         NODE_ENV: 'development'
       },
       env_production: {
         NODE_ENV: 'production'
-      }
+      },
+      max_restarts: 5,          // limit max restarts
+      restart_delay: 4000,
+      error_file: '/var/www/logs/koala-error.log',
+      out_file: '/var/www/logs/koala-out.log',
+      merge_logs: true,
     }]
   };
